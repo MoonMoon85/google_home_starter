e1fb47ed03d03fa0be1f08c5bad067f1b21e9765
Will Shalders <will@webmarketingroi.com.au>
2019-09-18 21:04:36 +1000

    Update sw1_off.py

--- public/python/sw1_off.py
+++ public/python/sw1_off.py
@@ -1,7 +1,11 @@
 # Edit line 6 to match your chosen GPIO pin
-print 'pyton off ran'
 import RPi.GPIO as GPIO
-GPIO.setwarnings(False)
+import time
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN)
-GPIO.cleanup()
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+print "LED on"
+GPIO.output(18,GPIO.HIGH)
+time.sleep(1)
+print "LED off"
+GPIO.output(18,GPIO.LOW)

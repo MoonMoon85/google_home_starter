d20d106fcf11a82fbcf951cfd64db7d44ffe9002
Will Shalders <will@webmarketingroi.com.au>
2019-09-18 21:17:52 +1000

    afafafa

--- public/python/sw1_off.py
+++ public/python/sw1_off.py
@@ -4,4 +4,5 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN)
+GPIO.output(21,GPIO.LOW)
 GPIO.cleanup()
--- public/python/sw1_on.py
+++ public/python/sw1_on.py
@@ -4,3 +4,4 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
+GPIO.output(21,GPIO.HIGH)
